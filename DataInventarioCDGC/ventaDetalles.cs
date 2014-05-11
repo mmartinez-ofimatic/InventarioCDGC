74f795ee2dd00e3ca6afd22836754a1b6a8d564c
mmartinez-ofimatic <mmartinez@ofimatic.com>
2014-05-11 11:51:14 -0400

    detalles

---
+++ DataInventarioCDGC/ventaDetalles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.EntityClient;
+
+namespace DataInventarioCDGC
+{
+    class ventaDetalles
+    {
+        InventarioCDGCEntities1 db = new InventarioCDGCEntities1();
+       
+    }
+}
