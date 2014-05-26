e8b61fea22ffcd76f87269bd4887f8b020edc240
mmartinez-ofimatic <mmartinez@ofimatic.com>
2014-05-26 17:41:14 -0400

    -Consulta venta

--- InventarioCDGC/Consultas/BuscarVentas.Designer.cs
+++ InventarioCDGC/Consultas/BuscarVentas.Designer.cs
@@ -31,16 +31,6 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BuscarVentasDetalles));
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
-            this.ColumnIDVenta = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.ColumnNombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.ColumnIDProducto = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.ColumnPrecio = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.ColumnCantidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.ColumnDescuento = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.nTotal = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.ColumnIDusuario = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.ColumnFecha = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.ColumnObservacion = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -48,6 +38,16 @@
             this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.label1 = new System.Windows.Forms.Label();
+            this.ColumnIDVenta = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnNombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnCantidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnIDProducto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnPrecio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnDescuento = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nTotal = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnIDusuario = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnFecha = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnObservacion = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
@@ -61,19 +61,18 @@
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.ColumnIDVenta,
             this.ColumnNombre,
+            this.ColumnCantidad,
             this.ColumnIDProducto,
             this.ColumnPrecio,
-            this.ColumnCantidad,
             this.ColumnDescuento,
             this.nTotal,
             this.ColumnIDusuario,
             this.ColumnFecha,
             this.ColumnObservacion});
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.dataGridView1.Location = new System.Drawing.Point(0, 80);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(682, 226);
+            this.dataGridView1.Size = new System.Drawing.Size(528, 226);
             this.dataGridView1.TabIndex = 40;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
@@ -94,87 +93,8 @@
             this.dataGridView2.Location = new System.Drawing.Point(0, 0);
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.ReadOnly = true;
-            this.dataGridView2.Size = new System.Drawing.Size(682, 42);
+            this.dataGridView2.Size = new System.Drawing.Size(681, 42);
             this.dataGridView2.TabIndex = 47;
-            // 
-            // ColumnIDVenta
-            // 
-            this.ColumnIDVenta.DataPropertyName = "ID_Venta";
-            this.ColumnIDVenta.HeaderText = "ID de Venta";
-            this.ColumnIDVenta.Name = "ColumnIDVenta";
-            this.ColumnIDVenta.ReadOnly = true;
-            this.ColumnIDVenta.Visible = false;
-            this.ColumnIDVenta.Width = 108;
-            // 
-            // ColumnNombre
-            // 
-            this.ColumnNombre.DataPropertyName = "Cliente";
-            this.ColumnNombre.HeaderText = "Cliente";
-            this.ColumnNombre.Name = "ColumnNombre";
-            this.ColumnNombre.ReadOnly = true;
-            this.ColumnNombre.Visible = false;
-            this.ColumnNombre.Width = 130;
-            // 
-            // ColumnIDProducto
-            // 
-            this.ColumnIDProducto.DataPropertyName = "Producto";
-            this.ColumnIDProducto.HeaderText = "Producto";
-            this.ColumnIDProducto.Name = "ColumnIDProducto";
-            this.ColumnIDProducto.ReadOnly = true;
-            this.ColumnIDProducto.Width = 110;
-            // 
-            // ColumnPrecio
-            // 
-            this.ColumnPrecio.DataPropertyName = "Precio_Unidad";
-            this.ColumnPrecio.HeaderText = "Precio de Venta";
-            this.ColumnPrecio.Name = "ColumnPrecio";
-            this.ColumnPrecio.ReadOnly = true;
-            this.ColumnPrecio.Width = 125;
-            // 
-            // ColumnCantidad
-            // 
-            this.ColumnCantidad.DataPropertyName = "Cantidad";
-            this.ColumnCantidad.HeaderText = "Cantidad";
-            this.ColumnCantidad.Name = "ColumnCantidad";
-            this.ColumnCantidad.ReadOnly = true;
-            // 
-            // ColumnDescuento
-            // 
-            this.ColumnDescuento.DataPropertyName = "Descuento";
-            this.ColumnDescuento.HeaderText = "Descuento";
-            this.ColumnDescuento.Name = "ColumnDescuento";
-            this.ColumnDescuento.ReadOnly = true;
-            // 
-            // nTotal
-            // 
-            this.nTotal.DataPropertyName = "Total";
-            this.nTotal.HeaderText = "Total";
-            this.nTotal.Name = "nTotal";
-            this.nTotal.ReadOnly = true;
-            // 
-            // ColumnIDusuario
-            // 
-            this.ColumnIDusuario.DataPropertyName = "Nombre";
-            this.ColumnIDusuario.HeaderText = "Vendedor";
-            this.ColumnIDusuario.Name = "ColumnIDusuario";
-            this.ColumnIDusuario.ReadOnly = true;
-            this.ColumnIDusuario.Visible = false;
-            // 
-            // ColumnFecha
-            // 
-            this.ColumnFecha.DataPropertyName = "Fecha";
-            this.ColumnFecha.HeaderText = "Fecha";
-            this.ColumnFecha.Name = "ColumnFecha";
-            this.ColumnFecha.ReadOnly = true;
-            this.ColumnFecha.Visible = false;
-            // 
-            // ColumnObservacion
-            // 
-            this.ColumnObservacion.DataPropertyName = "Observacion";
-            this.ColumnObservacion.HeaderText = "Observacion";
-            this.ColumnObservacion.Name = "ColumnObservacion";
-            this.ColumnObservacion.ReadOnly = true;
-            this.ColumnObservacion.Visible = false;
             // 
             // dataGridViewTextBoxColumn1
             // 
@@ -231,12 +151,93 @@
             this.label1.TabIndex = 48;
             this.label1.Text = "Detalles:";
             // 
+            // ColumnIDVenta
+            // 
+            this.ColumnIDVenta.DataPropertyName = "ID_Venta";
+            this.ColumnIDVenta.HeaderText = "ID de Venta";
+            this.ColumnIDVenta.Name = "ColumnIDVenta";
+            this.ColumnIDVenta.ReadOnly = true;
+            this.ColumnIDVenta.Visible = false;
+            this.ColumnIDVenta.Width = 108;
+            // 
+            // ColumnNombre
+            // 
+            this.ColumnNombre.DataPropertyName = "Cliente";
+            this.ColumnNombre.HeaderText = "Cliente";
+            this.ColumnNombre.Name = "ColumnNombre";
+            this.ColumnNombre.ReadOnly = true;
+            this.ColumnNombre.Visible = false;
+            this.ColumnNombre.Width = 130;
+            // 
+            // ColumnCantidad
+            // 
+            this.ColumnCantidad.DataPropertyName = "Cantidad";
+            this.ColumnCantidad.HeaderText = "Cantidad";
+            this.ColumnCantidad.Name = "ColumnCantidad";
+            this.ColumnCantidad.ReadOnly = true;
+            this.ColumnCantidad.Width = 70;
+            // 
+            // ColumnIDProducto
+            // 
+            this.ColumnIDProducto.DataPropertyName = "Producto";
+            this.ColumnIDProducto.HeaderText = "Producto";
+            this.ColumnIDProducto.Name = "ColumnIDProducto";
+            this.ColumnIDProducto.ReadOnly = true;
+            this.ColumnIDProducto.Width = 110;
+            // 
+            // ColumnPrecio
+            // 
+            this.ColumnPrecio.DataPropertyName = "Precio_Unidad";
+            this.ColumnPrecio.HeaderText = "Precio de Venta";
+            this.ColumnPrecio.Name = "ColumnPrecio";
+            this.ColumnPrecio.ReadOnly = true;
+            this.ColumnPrecio.Width = 125;
+            // 
+            // ColumnDescuento
+            // 
+            this.ColumnDescuento.DataPropertyName = "Descuento";
+            this.ColumnDescuento.HeaderText = "Descuento";
+            this.ColumnDescuento.Name = "ColumnDescuento";
+            this.ColumnDescuento.ReadOnly = true;
+            this.ColumnDescuento.Width = 80;
+            // 
+            // nTotal
+            // 
+            this.nTotal.DataPropertyName = "Total";
+            this.nTotal.HeaderText = "Total";
+            this.nTotal.Name = "nTotal";
+            this.nTotal.ReadOnly = true;
+            // 
+            // ColumnIDusuario
+            // 
+            this.ColumnIDusuario.DataPropertyName = "Nombre";
+            this.ColumnIDusuario.HeaderText = "Vendedor";
+            this.ColumnIDusuario.Name = "ColumnIDusuario";
+            this.ColumnIDusuario.ReadOnly = true;
+            this.ColumnIDusuario.Visible = false;
+            // 
+            // ColumnFecha
+            // 
+            this.ColumnFecha.DataPropertyName = "Fecha";
+            this.ColumnFecha.HeaderText = "Fecha";
+            this.ColumnFecha.Name = "ColumnFecha";
+            this.ColumnFecha.ReadOnly = true;
+            this.ColumnFecha.Visible = false;
+            // 
+            // ColumnObservacion
+            // 
+            this.ColumnObservacion.DataPropertyName = "Observacion";
+            this.ColumnObservacion.HeaderText = "Observacion";
+            this.ColumnObservacion.Name = "ColumnObservacion";
+            this.ColumnObservacion.ReadOnly = true;
+            this.ColumnObservacion.Visible = false;
+            // 
             // BuscarVentasDetalles
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(682, 306);
+            this.ClientSize = new System.Drawing.Size(681, 306);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.dataGridView1);
@@ -259,16 +260,6 @@
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataGridView dataGridView2;
-        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnIDVenta;
-        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnNombre;
-        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnIDProducto;
-        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnPrecio;
-        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnCantidad;
-        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnDescuento;
-        private System.Windows.Forms.DataGridViewTextBoxColumn nTotal;
-        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnIDusuario;
-        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnFecha;
-        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnObservacion;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
@@ -276,5 +267,15 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn6;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnIDVenta;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnNombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnCantidad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnIDProducto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnPrecio;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnDescuento;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nTotal;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnIDusuario;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnFecha;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnObservacion;
     }
 }
